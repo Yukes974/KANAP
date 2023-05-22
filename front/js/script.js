7671a1d2371e4e7d00f3a13de820410718ea25be
Patrick CHARLETTE <charlettepatrick@gmail.com>
2023-05-22 17:56:18 +0400

    Ajout des images de chaque produit, création éléments et leur liaison
    Les images de chaque produit sont mises dans le dossier "images" du front temporairement. Création dynamique des fiches produits depuis le js vers le html et liaison de la page "index.html" à "produit.html".

--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,42 @@
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("../../product.json");
-const products = await reponse.json();
+// Récupération des produits depuis le fichier JSON
+fetch("/js/product.json")
+    .then(response => response.json())
+    .then(products => {
+    
 
-const article = products[0];
-const imageUrlElement = document.createElement("img");
-imageUrlElement.src = article.imageUrl;
-const productNameElement = document.createElement("h3");
-productNameElement.innerText = article.name;
-const productDescriptionElement = document.createElement("p");
-productDescriptionElement.innerText = article.description;
+    for (let i = 0; i < products.length; i++) {
 
+        // Récupération de l'élément du DOM qui accueillera les items
+        const sectionFiches = document.querySelector(".items");
+        // Création d'un lien pour chaque produit
+        const linkElement = document.createElement("a");
+        // Direction du lien
+        linkElement.href = "/html/product.html";
+        // Création d’une balise dédiée à un produit
+        const productsElement = document.createElement("article");
+        // On crée l’élément img.
+        const imageUrlElement = document.createElement("img");
+        // On accède à l’indice i de la liste products pour configurer la source de l’image.
+        imageUrlElement.src = products[i].imageUrl;
+        // Idem pour le nom et la description...
+        const productNameElement = document.createElement("h3")
+        productNameElement.innerText = products[i].name;
+        const productDescriptionelement = document.createElement("p");
+        productDescriptionelement.innerText = products[i].description;
 
-const sectionFiches = document.querySelector(".items");
-sectionFiches.appendChild(imageUrlElement);
-sectionFiches.appendChild(productNameElement);
-sectionFiches.appendChild(productDescriptionElement);
+        
+        
+        // On rattache la balise "a" à la section Items
+        sectionFiches.appendChild(linkElement);
+        // On rattache l'article à la balise "a"
+        linkElement.appendChild(productsElement);
+        // On rattache l’image à productsElement (la balise article)
+        productsElement.appendChild(imageUrlElement);
+        // Idem pour le nom et la description...
+        productsElement.appendChild(productNameElement);
+        productsElement.appendChild(productDescriptionelement);
+        
+        }
+
+    });
+    
