9c59b27b8b94aa5ec15e4edc7e7c60a54ea2b6c3
Patrick CHARLETTE <charlettepatrick@gmail.com>
2023-05-29 18:03:42 +0400

    Insertion d'un produit et ses détails dans "product.js"

--- front/js/product.js
+++ front/js/product.js
@@ -2,9 +2,30 @@ fetch("/js/product.json")
   .then((response) => response.json())
   .then((products) => {
     let str = new URL(window.location.href);
-    console.log(str);
     let url = new URLSearchParams(str.search);
-    console.log(url.has("id"));
     let id = url.get("id");
-    console.log(id);
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i]._id == id) {
+        const imageUrl = `<img src="${products[i].imageUrl}" alt="${products[i].altTxt}"/>`;
+        const classItemImg = document.querySelector(".item__img");
+        classItemImg.insertAdjacentHTML("beforeEnd", imageUrl);
+        const productName = `<h1>${products[i].name}</h1>`;
+        const idTitle = document.querySelector("#title");
+        idTitle.insertAdjacentHTML("beforeEnd", productName);
+        const productPrice = `<span>${products[i].price}</span>`;
+        const idPrice = document.querySelector("#price");
+        idPrice.insertAdjacentHTML("beforeEnd", productPrice);
+        const productDescription = `<p>${products[i].description}</p>`;
+        const idDescription = document.querySelector("#description");
+        idDescription.insertAdjacentHTML("beforeEnd", productDescription);
+        const productColor1 = `<option>${products[i].colors[0]}</option>`;
+        const productColor2 = `<option>${products[i].colors[1]}</option>`;
+        const productColor3 = `<option>${products[i].colors[2]}</option>`;
+        const idColors = document.querySelector("#colors");
+        idColors.insertAdjacentHTML("beforeEnd", productColor1);
+        idColors.insertAdjacentHTML("beforeEnd", productColor2);
+        idColors.insertAdjacentHTML("beforeEnd", productColor3);
+      }
+    }
   });
